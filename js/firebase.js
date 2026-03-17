2f9f3e854a9ee518f36120720f2cbc5768c2a7e3
sunilkumarm048 <55477491+sunilkumarm048@users.noreply.github.com>
2026-03-17 09:15:34 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,14 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
 const firebaseConfig = {
-apiKey:"YOUR_KEY",
-authDomain:"YOUR_PROJECT.firebaseapp.com",
-projectId:"YOUR_PROJECT"
+ apiKey: "AIzaSyDiENWyBsHdVCNszjPb_gKT_JqBWxkKPfU",
+ authDomain: "local-shop-orders-d48d0.firebaseapp.com",
+ projectId: "local-shop-orders-d48d0",
+ storageBucket: "local-shop-orders-d48d0.firebasestorage.app",
+ messagingSenderId: "750524308284",
+ appId: "1:750524308284:web:d2a047c93580635e4e5c39"
 };
 
 const app = initializeApp(firebaseConfig);
