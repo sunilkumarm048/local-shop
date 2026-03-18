e081b9124d5ac02f870a1040d95773334f7585b6
sunilkumarm048 <55477491+sunilkumarm048@users.noreply.github.com>
2026-03-18 14:06:18 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
- apiKey: "AIzaSyDiENWyBsHdVCNszjPb_gKT_JqBWxkKPfU",
- authDomain: "local-shop-orders-d48d0.firebaseapp.com",
- projectId: "local-shop-orders-d48d0",
- storageBucket: "local-shop-orders-d48d0.firebasestorage.app",
- messagingSenderId: "750524308284",
- appId: "1:750524308284:web:d2a047c93580635e4e5c39"
+apiKey: "AIzaSyDiENWyBsHdVCNszjPb_gKT_JqBWxkKPfU",
+authDomain: "local-shop-orders-d48d0.firebaseapp.com",
+projectId: "local-shop-orders-d48d0",
+storageBucket: "local-shop-orders-d48d0.appspot.com",
+messagingSenderId: "750524308284",
+appId: "1:750524308284:web:d2a047c93580635e4e5c39"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-export const auth = getAuth(app);
